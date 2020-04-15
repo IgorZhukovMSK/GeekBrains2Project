1f59dc09c9280ca65937cf83d2e02fd74631d0af
IgorZhukovMSK <vot_tak78@mail.ru>
2020-04-15 12:07:59 +0300

    Написание классов (код на основе занятия он-лайн)

--- src/Level2/Lesson4/ClientGUI.java
+++ src/Level2/Lesson4/ClientGUI.java
@@ -1,9 +1,95 @@
 package Level2.Lesson4;
 
-public class ClientGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    public static void main (String [] args){
-        
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAdress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
+    private final JTextField tfLogin = new JTextField("Igor");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+
+    ClientGUI() {
+
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setAlwaysOnTop(true);
+        userList.setListData(new String[]{"user1", "user2", "user3", "user4", "user5",
+                "user6", "user7", "user8", "user9", "user0",});
+        JScrollPane scrlUser = new JScrollPane(userList);
+        JScrollPane scrlPane = new JScrollPane(log);
+        scrlUser.setPreferredSize(new Dimension(100, 0));
+        log.setWrapStyleWord(true);
+        log.setEnabled(false);
+        cbAlwaysOnTop.addActionListener(this);
+
+        panelTop.add(tfIPAdress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrlUser, BorderLayout.EAST);
+        add(scrlPane, BorderLayout.CENTER);
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+
+        } else {
+            throw new RuntimeException("Unknown sourse: " + src);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = String.format("Exception in \"%s\" %s: %s\n\t at %s", t.getName(), e.getClass().getCanonicalName(), e.getMessage(), ste[0]);
+        JOptionPane.showMessageDialog(this, msg, "Exceptin", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
     }
 
 }
+
+
--- src/Level2/Lesson4/ServerGUI.java
+++ src/Level2/Lesson4/ServerGUI.java
@@ -1,4 +1,67 @@
 package Level2.Lesson4;
 
-public class ServerGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ServerGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+    private static final int POS_X = 1000;
+    private static final int POS_Y = 550;
+    private static final int WIDTH = 200;
+    private static final int HEIGHT = 100;
+    private final ChatServer server;
+    private final JButton buttonStart = new JButton("Start");
+    private final JButton buttonStop = new JButton("Stop");
+
+    ServerGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat server");
+        setAlwaysOnTop(true);
+        setLayout(new GridLayout(1, 2));
+        buttonStart.addActionListener(this);
+        buttonStop.addActionListener(this);
+
+
+        add(buttonStart);
+        add(buttonStop);
+        server = new ChatServer();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ServerGUI();
+            }
+        });
+        // throw new RuntimeException("Main died!!!!");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == buttonStart) {
+            server.start(8189);
+        } else if (src == buttonStop) {
+            //  server.stop();
+            throw new RuntimeException("Hello from EDT!!");
+        } else {
+            throw new RuntimeException("Unknown sourse: " + src);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = String.format("Exception in \"%s\" %s: %s\n\t at %s", t.getName(), e.getClass().getCanonicalName(), e.getMessage(), ste[0]);
+        JOptionPane.showMessageDialog(this, msg, "Exceptin", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
 }
