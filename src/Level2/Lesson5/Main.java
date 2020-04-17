eec4afb8a397d2b590ccf20ad626369df1c54cd3
IgorZhukovMSK <vot_tak78@mail.ru>
2020-04-17 15:26:51 +0300

    Создание массива, заполнение его единицами

--- src/Level2/Lesson5/Main.java
+++ src/Level2/Lesson5/Main.java
@@ -1,4 +1,35 @@
 package Level2.Lesson5;
+/*
+
+1. Необходимо написать два метода, которые делают следующее:
+
+1) Создают одномерный длинный массив, например:
+
+static final int size = 10000000;
+static final int h = size / 2;
+float[] arr = new float[size];
+
+2) Заполняют этот массив единицами;
+3) Засекают время выполнения: long a = System.currentTimeMillis();
+
+ */
 
 public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+
+    public static void main (String [] args){
+
+        float[] arr = new float[size];
+
+        long a = System.currentTimeMillis();
+
+        for (float fl : arr) {
+
+            System.out.println(fl);
+        }
+
+    }
+
 }
